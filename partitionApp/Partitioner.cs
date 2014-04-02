8f29bcd1bb0792280ac497e31e898be3066241a0
mikevlaar <udgement1>
2014-04-02 19:43:40 +0200

    commit

---
+++ partitionApp/Partitioner.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partitionApp
+{
+    class Partitioner
+    {
+        public int numberOfPartitions = 0;
+
+        public void partition(int n, int max, String prefix)
+        {
+            if (n == 0)
+            {
+                Console.WriteLine(prefix);
+                numberOfPartitions++;
+                return;
+            }
+
+            for (int i = Math.Min(n,max); i >= 1; i--)
+            {
+                partition(n - i, i, prefix + " " + i);
+            }
+        }
+
+        public void printPartitions(int target, int max, String suffix)
+        {
+            if (target == 0)
+               Console.WriteLine(suffix);
+            else
+            {
+                for (int i = 1; i <= max && i <= target; i++)
+                {
+                    printPartitions(target - i, i, i + " " + suffix);
+                }
+            }
+        }
+    }
+}
---
+++ partitionApp/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace partitionApp
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Partitioner partitioner = new Partitioner();
+            int numValue = -1;
+            bool repeat = true;
+
+            while (repeat == true)
+            {
+                Console.WriteLine("Enter a positive int number: ");
+                string input = Console.ReadLine();
+
+                try
+                {
+                    numValue = Convert.ToInt32(input);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine("Input string is not a sequence of digits.");
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine("The number cannot fit in an Int32.");
+                }
+                finally
+                {
+                    if (numValue < Int32.MaxValue)
+                    {
+                        //partitioner.printPartitions(numValue, numValue, "");
+                        partitioner.partition(numValue, numValue, "");
+                        Console.WriteLine("number of lines:" + partitioner.numberOfPartitions);
+                    }
+                    else
+                    {
+                        Console.WriteLine("please enter a valid number");
+                    }
+                }
+
+                Console.WriteLine("Go again? Y/N");
+                string go = Console.ReadLine();
+                if (go == "Y" || go == "y")
+                {
+                    repeat = true;
+                }
+                else
+                {
+                    repeat = false;
+                }
+            }
+        }
+    }
+}
